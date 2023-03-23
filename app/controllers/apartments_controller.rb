8c3d2a8a7a0a6a3657499aae7c26d7cbf420ff57
loretta dhahabu <loreydjefwa@gmail.com>
2023-03-23 11:57:27 +0300

    get and update apartments details

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,2 +1,41 @@
 class ApartmentsController < ApplicationController
-end
+    rescue_from ActiveRecord::RecordNotFound, with: :apartment_not_found
+
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+  
+    skip_before_action :authenticate, only: [:index, :show, :create, :update]
+  
+    # GET /apartments
+    def index
+      apartments = Apartment.all
+      render json: apartments
+    end
+  
+    #  GET /apartments/:id
+    def show
+      apartment = Apartment.find(params[:id])
+      render json: apartment, serializer: ApartmentRoomSerializer, status: :ok
+    end
+  
+    # PATCH /apartments/:id
+    def update
+      apartment = Apartment.find(params[:id])
+      apartment.update!(apartment_params)
+      render json: apartment
+    end
+  
+    private
+  
+    def apartment_params
+      params.require(:apartment).permit(:id, :name, :image, :description, :price)
+    end
+  
+    def unprocessable_entity(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+  
+    def apartment_not_found
+      render json: { error: "apartment not found" }, status: :not_found
+    end
+  end
+  
