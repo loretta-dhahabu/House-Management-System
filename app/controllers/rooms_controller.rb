802f36c02753f17f6b2dca39eb0d8f8315a191f4
loretta dhahabu <loreydjefwa@gmail.com>
2023-03-23 12:01:04 +0300

    get ll accomodated and empty rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,56 @@
 class RoomsController < ApplicationController
-end
+    rescue_from ActiveRecord::RecordNotFound, with: :room_not_found
+
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+  
+    skip_before_action :authenticate, only: [:index, :show, :create]
+    
+    # GET /rooms
+    def index
+      rooms = Room.all
+      render json: rooms
+    end
+  
+    #  GET /rooms/:id
+    def show
+      room = Room.find(params[:id])
+      render json: room,  status: :ok
+    end
+  
+    # POST/rooms
+    def create
+      room = Room.create!(room_params)
+      render json: room, status: :created
+    end
+  
+    # PATCH /rooms/:id
+    def update
+      #find
+      room = Room.find(params[:id])
+      room.update!(room_params)
+      render json: room
+    end
+  
+    # DELETE /rooms/:id
+    def destroy
+      # find
+      room = Room.find(params[:id])
+      # delete
+      room.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def room_params
+      params.permit(:id, :room_no, :apartment_id)
+    end
+  
+    def unprocessable_entity(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+  
+    def room_not_found
+      render json: { error: "room not found" }, status: :not_found
+    end
+  end
